1cf35b8991d1fcbc7e786034b9ba9600be465106
Divakar <divakartamanara@gmail.com>
2026-03-28 12:26:23 +0530

    Hackfest NareshIT 2026 - Initial Project Setup

---
+++ Backend Code/rapidcare-backend/src/main/java/com/rapidcare/backend/RapidcareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rapidcare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidcareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidcareBackendApplication.class, args);
+	}
+
+}
---
+++ Backend Code/rapidcare-backend/src/test/java/com/rapidcare/backend/RapidcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidcare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
